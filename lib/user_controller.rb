9e9dc5a8dcecae041d3a7004750d6669ef866460
Donatos Stavropoulos <dostavro@gmail.com>
2013-06-21 19:59:54 +0300

    Added a configuration file

--- lib/user_controller.rb
+++ lib/user_controller.rb
@@ -1,8 +1,13 @@
 require 'omf_rc'
 require 'omf_common'
+require 'yaml'
 
 $stdout.sync = true
 
+
+@config = YAML.load_file('../etc/configuration.yaml')
+@auth = @config[:auth]
+@xmpp = @config[:xmpp]
 
 module OmfRc::ResourceProxy::UserController
   include OmfRc::ResourceProxyDSL
@@ -165,12 +170,15 @@ module OmfRc::ResourceProxy::User
   end
 end
 
-entity = OmfCommon::Auth::Certificate.create_from_x509(File.read("/home/dostavro/.omf/urc.pem"),
-                                                       File.read("/home/dostavro/.omf/user_rc_key.pem"))
+entity_cert = File.expand_path(@auth[:entity_cert])
+entity_key = File.expand_path(@auth[:entity_key])
+entity = OmfCommon::Auth::Certificate.create_from_x509(File.read(entity_cert), File.read(entity_key))
 
-OmfCommon.init(:development, communication: { url: 'xmpp://alpha:pw@localhost', auth: {} }) do
+trusted_roots = File.expand_path(@auth[:root_cert_dir])
+
+OmfCommon.init(:development, communication: { url: "xmpp://#{@xmpp[:username]}:#{@xmpp[:password]}@#{@xmpp[:server]}", auth: {} }) do
   OmfCommon.comm.on_connected do |comm|
-    OmfCommon::Auth::CertificateStore.instance.register_default_certs("/home/dostavro/.omf/trusted_roots/")
+    OmfCommon::Auth::CertificateStore.instance.register_default_certs(trusted_roots)
     OmfCommon::Auth::CertificateStore.instance.register(entity, OmfCommon.comm.local_topic.address)
     OmfCommon::Auth::CertificateStore.instance.register(entity)
 
